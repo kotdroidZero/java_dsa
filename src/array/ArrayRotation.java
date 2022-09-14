0ff838039489ad8bffece4c2b26c85160b7cd229
pushkar45756 <pushkar.srivastava@infostride.com>
2022-09-15 04:05:15 +0530

    array rotation - way2 and way3

--- src/array/ArrayRotation.java
+++ src/array/ArrayRotation.java
@@ -36,3 +36,36 @@ public class ArrayRotation {
         return  result;
     }
 }
+
+
+
+// other approaches
+// 1.
+/* Rotate one by one
+Let us take arr[] = [1, 2, 3, 4, 5, 6, 7], d = 2.
+
+        First Step:
+        => Rotate to left by one position.
+        => arr[] = {2, 3, 4, 5, 6, 7, 1}
+
+        Second Step:
+        => Rotate again to left by one position
+        => arr[] = {3, 4, 5, 6, 7, 1, 2}
+
+        Rotation is done by 2 times.
+        So the array becomes arr[] = {3, 4, 5, 6, 7, 1, 2}*/
+
+//2. Using Temp Array
+/*Suppose the give array is arr[] = [1, 2, 3, 4, 5, 6, 7], d = 2.
+
+        First Step:
+        => Store the elements from 2nd index to the last.
+        => temp[] = [3, 4, 5, 6, 7]
+
+        Second Step:
+        => Now store the first 2 elements into the temp[] array.
+        => temp[] = [3, 4, 5, 6, 7, 1, 2]
+
+        Third Steps:
+        => Copy the elements of the temp[] array into the original array.
+        => arr[] = temp[] So arr[] = [3, 4, 5, 6, 7, 1, 2]*/
